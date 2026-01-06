cdb819983fbfc9d7d5b489b1d5a250700f16d93e
akshaygite24 <akshaygite2004@gmail.com>
2026-01-06 12:40:26 +0530

    Add question model

--- answerly/apps/questions/models.py
+++ answerly/apps/questions/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+User = settings.AUTH_USER_MODEL
+
+class Question(models.Model):
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='questions'
+    )
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
