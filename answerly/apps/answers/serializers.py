1fcb1c148a8a526a0b814ac29368813b96aeca97
akshaygite24 <akshaygite2004@gmail.com>
2026-01-06 14:32:49 +0530

    Add vote counts to questions and answers

--- answerly/apps/answers/serializers.py
+++ answerly/apps/answers/serializers.py
@@ -1,9 +1,29 @@
 from rest_framework import serializers
+from django.contrib.contenttypes.models import ContentType
+from apps.votes.models import Vote
 from .models import Answer
 
 class AnswerSerializer(serializers.ModelSerializer):
     author = serializers.ReadOnlyField(source='author.username')
+    upvotes = serializers.SerializerMethodField()
+    downvotes = serializers.SerializerMethodField()
 
     class Meta:
         model = Answer
         fields = '__all__'
+
+    def get_upvotes(self, obj):
+        content_type = ContentType.objects.get_for_model(Answer)
+        return Vote.objects.filter(
+            content_type=content_type,
+            object_id=obj.id,
+            value=1
+        ).count()
+    
+    def get_downvotes(self, obj):
+        content_type = ContentType.objects.get_for_model(Answer)
+        return Vote.objects.filter(
+            content_type=content_type,
+            object_id=obj.id,
+            value=-1
+        ).count()
--- answerly/apps/questions/serializers.py
+++ answerly/apps/questions/serializers.py
@@ -1,9 +1,29 @@
 from rest_framework import serializers
 from .models import Question
+from apps.votes.models import Vote
+from django.contrib.contenttypes.models import ContentType
 
 class QuestionSerializer(serializers.ModelSerializer):
     author = serializers.ReadOnlyField(source='author.username')
-    
+    upvotes = serializers.SerializerMethodField()
+    downvotes = serializers.SerializerMethodField()
+
     class Meta:
         model = Question
         fields = '__all__'
+
+    def get_upvotes(self, obj):
+        content_type = ContentType.objects.get_for_model(Question)
+        return Vote.objects.filter(
+            content_type=content_type,
+            object_id=obj.id,
+            value=1
+        ).count()
+    
+    def get_downvotes(self, obj):
+        content_type = ContentType.objects.get_for_model(Question)
+        return Vote.objects.filter(
+            content_type=content_type,
+            object_id=obj.id,
+            value=-1
+        ).count()
