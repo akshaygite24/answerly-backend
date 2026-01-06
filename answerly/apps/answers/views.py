e70bfd3ff836f5ee6e6a3671c313b5ed824ecd8b
akshaygite24 <akshaygite2004@gmail.com>
2026-01-06 13:57:12 +0530

    Filter answers by question

--- answerly/apps/answers/views.py
+++ answerly/apps/answers/views.py
@@ -8,5 +8,14 @@ class AnswerViewSet(ModelViewSet):
     serializer_class = AnswerSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
 
+    def get_queryset(self):
+        queryset = Answer.objects.all()
+        question_id = self.request.query_params.get('question')
+
+        if question_id:
+            queryset = queryset.filter(question_id=question_id)
+
+        return queryset
+
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
