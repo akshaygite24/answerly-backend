93c383c95374827d5dcb0ee5c2d9d2bcfe82f5e3
akshaygite24 <akshaygite2004@gmail.com>
2026-01-06 12:50:51 +0530

    Create answer model linked to questions

--- answerly/apps/answers/models.py
+++ answerly/apps/answers/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.conf import settings
+from apps.questions.models import Question
 
-# Create your models here.
+User = settings.AUTH_USER_MODEL
+
+class Answer(models.Model):
+    question = models.ForeignKey(
+        Question,
+        on_delete=models.CASCADE,
+        related_name='answers'
+    )
+    author = models.ForeignKey(
+        User, 
+        on_delete=models.CASCADE,
+        related_name='answers'
+    )
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Answer by {self.author} on Question {self.question.id}'
+
