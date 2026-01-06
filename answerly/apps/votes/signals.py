79f1964bb205fe62c06a3d05449cc72ec1aaf08f
akshaygite24 <akshaygite2004@gmail.com>
2026-01-06 15:24:05 +0530

    Ensure user profile exists before updating reputation

--- answerly/apps/votes/signals.py
+++ answerly/apps/votes/signals.py
@@ -1,14 +1,15 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import Vote
+from apps.accounts.models import Profile
 
 @receiver(post_save, sender=Vote)
 def update_reputation_on_vote(sender, instance, created, **kwargs):
     owner = instance.content_object.author
-    profile = owner.profile
+    
+    profile, _ = Profile.objects.get_or_create(user=owner)
 
-
-    # created or updated vote
+    # update reputation based on vote value
     if instance.value == 1:
         profile.reputation += 10
     elif instance.value == -1:
