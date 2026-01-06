30de04baf6747261f979ae06314a82d39c544f98
akshaygite24 <akshaygite2004@gmail.com>
2026-01-06 12:29:13 +0530

    Add user profile and reputation model

--- answerly/apps/accounts/models.py
+++ answerly/apps/accounts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+User = settings.AUTH_USER_MODEL
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    reputation = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.user.username
