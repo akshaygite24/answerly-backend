329c0c6db116f62563260c2e50e5258f9ba13dc7
akshaygite24 <akshaygite2004@gmail.com>
2026-01-07 13:32:34 +0530

    Add tag based filtering for questions

--- answerly/apps/questions/views.py
+++ answerly/apps/questions/views.py
@@ -11,3 +11,12 @@ class QuestionViewSet(ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+
+    def get_queryset(self):
+        queryset = Question.objects.all()
+        
+        tag = self.request.query_params.get('tag')
+        if tag:
+            queryset = queryset.filter(tags__name=tag.lower())
+
+        return queryset
