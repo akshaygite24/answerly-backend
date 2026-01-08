a3e0a7086f64cdad369896b4d85f5a4a422b3312
akshaygite24 <akshaygite2004@gmail.com>
2026-01-08 11:47:29 +0530

    Enable auto login after user registration

--- answerly/apps/accounts/views.py
+++ answerly/apps/accounts/views.py
@@ -3,6 +3,7 @@ from rest_framework.generics import RetrieveAPIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework_simplejwt.tokens import RefreshToken
 from .serializers import ProfileSerializer, RegisterSerializer
 from .models import Profile
 
@@ -29,13 +30,23 @@ class RegisterAPIView(APIView):
         
         if serializer.is_valid():
             user = serializer.save()
+
+            refresh = RefreshToken.for_user(user)
+
             return Response(
                 {
                     "message": "User registered successfully.",
-                    "id": user.id,
-                    "username": user.username,
-                    "email": user.email,
-                }, status=status.HTTP_201_CREATED
+                    "user": {
+                        "id": user.id,
+                        "username": user.username,
+                        "email": user.email,
+                    },
+                    "token": {
+                    "refresh": str(refresh),
+                    "access": str(refresh.access_token),
+                    },
+                },status=status.HTTP_201_CREATED,
             )
+        
         print(serializer.errors)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
