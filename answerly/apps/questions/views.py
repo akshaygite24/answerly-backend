a71dd054480a89c2816c38192df35b1a8dddacfc
akshaygite24 <akshaygite2004@gmail.com>
2026-01-08 14:30:51 +0530

    Add Search filter for questions

--- answerly/apps/questions/views.py
+++ answerly/apps/questions/views.py
@@ -3,11 +3,16 @@ from .models import Question
 from .serializers import QuestionSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from apps.common.permissions import IsAuthorOrReadOnly
+from rest_framework.filters import SearchFilter
 
 class QuestionViewSet(ModelViewSet):
     queryset = Question.objects.all()
     serializer_class = QuestionSerializer
     permission_classes = [IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
+
+    # Search functionality
+    filter_backends = [SearchFilter]
+    search_fields = ['title', 'body', 'tags__name']
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
@@ -19,4 +24,4 @@ class QuestionViewSet(ModelViewSet):
         if tag:
             queryset = queryset.filter(tags__name=tag.lower())
 
-        return queryset.order_by('created_at')
+        return queryset.order_by('-created_at')
